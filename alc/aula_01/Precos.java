650f71179bb5c7e52c7ee140529e9ead77f12187
João Pedro <jpedrones@hotmail.com>
2023-03-18 08:29:45 -0300

    update

---
+++ alc/aula_01/Precos.java
@@ -0,0 +1,21 @@
+/*
+ *	11/03/2023
+ *
+ *	Um programa que recebe o preço de um produto e o deconto em (%)
+ *	Calcular o preço do disconto e o valor final
+*/
+//=========================================================================================================
+package fatec.alc.aula_01;
+import	javax.swing.JOptionPane;
+
+public class Precos{
+	public static void main(String[] args) {
+		double price, percent, finalValue, discount;
+		price = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do produto:"));
+		percent = Double.parseDouble(JOptionPane.showInputDialog("Digite a % do desconto:"));
+
+		discount = price * (percent / 100);
+		finalValue = price - discount;
+		JOptionPane.showMessageDialog(null, "Valor final: " + finalValue + "\nDesconto aplicado: " + discount);
+	}
+}
