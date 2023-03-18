781fdc6ebfea9610807a2d6f2c7e6a2fc369588e
João Pedro <jpedrones@hotmail.com>
2023-03-18 10:24:10 -0300

    Add: checkNumber.java

---
+++ alc/aula_02/checkNumber.java
@@ -0,0 +1,33 @@
+/*
+ *	18/03/2023
+ *
+*/
+//=========================================================================================================
+
+package fatec.alc.aula_02;
+import	java.util.Scanner;
+
+public class checkNumber {
+	public static void main(String[] args) {
+		double	nb;
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite um número: ");
+		nb = scanner.nextDouble();
+
+		//Teste se é positivo
+		if (nb > 0)
+			System.out.println("É um número positivo ~(￣▽￣)~");
+		else if (nb < 0)
+			System.out.println("É um número negativo `(*>﹏<*)′");
+		else
+			System.out.println("É um número Nulo (╯‵□′)╯︵┻━┻");
+
+		//teste se é ímpar
+		if (nb % 2 == 0 && nb != 0)
+			System.out.println("O número é par ~(￣▽￣)~");
+		else if (nb != 0)
+			System.out.println("O número é ímpar `(*>﹏<*)′");
+		scanner.close();
+	}
+}
